0c8725227c474501c3f24f744e6fda8e327c0ce2
KTVo <foolycooly288622@yahoo.com>
2020-10-19 17:04:03 -0700

    some required tasked included

---
+++ bernieanimation/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "../../../WebstormProjects/bernieanimation/src/views/header";
+import Body from "../../../WebstormProjects/bernieanimation/src/views/body";
+import "../../../WebstormProjects/bernieanimation/src/assets/App.scss";
+
+function App() {
+  return (
+      <div id="App">
+        <Header></Header>
+        <Body></Body>
+      </div>
+  );
+}
+
+export default App;
