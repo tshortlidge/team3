26d8dcfc1f68273fe7b759a41b650b514a36a048
KTVo <foolycooly288622@yahoo.com>
2020-11-05 12:56:52 -0800

    attempted to include fetch for Bernie's cards

--- secondchancefrontend/src/components/data/data.js
+++ secondchancefrontend/src/components/data/data.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PicOfDoc1 from '../images/dr_female/female_quality_1/female_doctor_1.jpg';
 import PicOfDoc2 from '../images/dr_female/female_quality_1/female_doctor_2.jpg';
 import PicOfDoc3 from '../images/dr_female/female_quality_1/female_doctor_3.jpg';
@@ -9,6 +10,68 @@ import PicOfDoc8 from '../images/dr_male/male_quality_1/male_doctor_2.jpg';
 import PicOfDoc9 from '../images/dr_male/male_quality_1/male_doctor_3.jpg';
 
 
+//export let people1 = [];
+
+
+
+
+
+export function GrabDrCardInfo(props)
+{
+
+  //Calls to endpoint for every doctors' info
+  //then push into people1 array
+    console.log("Testing get_all_physician_records POST");
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({"phy_id":1})
+    };
+
+    fetch("http://52.247.220.137:80/get_all_physician_records",requestOptions)
+        .then(response => response.json())
+        .then(
+            (result) =>
+            {
+
+              /*
+* name: "Dr.Stevens",
+    about: "",
+    age: 46,
+    image: PicOfDoc1,
+    location: "Ronald Reagan UCLA Medical Center",
+    email: "somedoctor@medicalplace.com",
+    rating: "4.5",
+    npi:"0",
+    specialty:"Orthopedic surgeon"
+* */
+
+              let l = result.length;
+              let peopleArray = [];
+              for(let i = 0; i < l; i++)
+              {
+                peopleArray[i].name = result[i].name;
+                peopleArray[i].age = Math.random()%30+30;
+                peopleArray[i].location = result[i].addr;
+                peopleArray[i].email = result[i].email;
+                peopleArray[i].rating = "CONSULT BACKEND ABOUT A FLOAT RATING [0.0, 5.0]";
+                peopleArray[i].npi = result[i].npi;
+                peopleArray[i].specialty = result[i].qual;
+                peopleArray[i].drId = result[i].phy_id;
+
+              }
+              this.props.processPeople(peopleArray);
+
+            }
+        );
+
+
+
+
+
+}
+
+/*
 export const people1 = [
   {
     name: "Dr.Stevens",
@@ -111,4 +174,4 @@ export const people1 = [
   }
 ];
 
-
+*/
--- secondchancefrontend/src/components/data/fetch.js
+++ secondchancefrontend/src/components/data/fetch.js
@@ -1,5 +1,45 @@
 import { people1 } from "./data.js";
 
+
+export function GrabDrCardInfo(props)
+{
+
+  //Calls to endpoint for every doctors' info
+  //then push into people1 array
+  console.log("Testing get_all_physician_records POST");
+  const requestOptions = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({"phy_id":1})
+  };
+
+  fetch("http://52.247.220.137:80/get_all_physician_records",requestOptions)
+      .then(response => response.json())
+      .then(
+          (result) =>
+          {
+
+            let l = result.length;
+            let peopleArray = [];
+            for(let i = 0; i < l; i++)
+            {
+              peopleArray[i].name = result[i].name;
+              peopleArray[i].age = Math.random()%30+30;
+              peopleArray[i].location = result[i].addr;
+              peopleArray[i].email = result[i].email;
+              peopleArray[i].rating = "CONSULT BACKEND ABOUT A FLOAT RATING [0.0, 5.0]";
+              peopleArray[i].npi = result[i].npi;
+              peopleArray[i].specialty = result[i].qual;
+              peopleArray[i].drId = result[i].phy_id;
+
+            }
+            this.props.processPeople(peopleArray);
+
+          }
+      );
+
+}
+
 const FromList = (list, index) => {
   return list[index];
 };
